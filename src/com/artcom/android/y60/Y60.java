a2ec10c1a508bdd430e6ee39b133b2e176ae780a
Rodja Trappe <rodja@localhost.(none)>
2009-02-10 12:21:22 +0100

    setup a fresh eclipse project

---
+++ src/com/artcom/android/y60/Y60.java
@@ -0,0 +1,13 @@
+package com.artcom.android.y60;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Y60 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
