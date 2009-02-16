f346d2f457d04b99067f16da3424e1ee85b6273b
Rodja Trappe <rodja@artcom.de>
2009-02-16 16:02:19 +0100

    merged from master

--- src/com/artcom/y60/infrastructure/GOMClient.java
+++ src/com/artcom/y60/infrastructure/GOMClient.java
@@ -1,57 +1,12 @@
-/*
- *  Copyright (C) 1993-2008, ART+COM AG Berlin, Germany <www.artcom.de>
- * 
- *  These coded instructions, statements, and computer programs contain
- *  proprietary information of ART+COM AG Berlin, and are copy protected
- *  by law. They may be used, modified and redistributed under the terms
- *  of GNU General Public License referenced below. 
- *     
- *  Alternative licensing without the obligations of the GPL is
- *  available upon request.
- * 
- *  GPL v3 Licensing:
- * 
- *  This file is part of the ART+COM Y60 Platform.
- * 
- *  ART+COM Y60 is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  ART+COM Y60 is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with ART+COM Y60.  If not, see <http: * www.gnu.org/licenses/>.
- */
+package com.artcom.y60.infrastructure.gom.android;
 
-package com.artcom.y60.infrastructure;
+import java.util.Vector;
 
-import java.io.IOException;
-import java.util.Iterator;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
-import android.content.ContentProvider;
-import android.content.ContentValues;
 import android.content.UriMatcher;
-import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
-public class GOMClient extends ContentProvider {
-
-	private static final String myURI = "com.artcom.y60.infrastructure.gom";
-	public static final Uri CONTENT_URI = Uri.parse( myURI );
-	
+public class GOMClient
+{
 	private static final int URI_TYPE_NODE  = 1;
 	private static final int URI_TYPE_ENTRY = 2;
 	
@@ -63,54 +18,28 @@ public class GOMClient extends ContentProvider {
 		uriMatcher.addURI( "com.artcom.y60.infrastructure.gom", "/*:*", URI_TYPE_ENTRY );
 	}
 	
-	@Override
-	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getType(Uri uri) {
+	public GOMClient()
+	{
 		
-
-		
-		return null;
 	}
-
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
-		
-		switch (uriMatcher.match( uri ))
-		{
-		case URI_TYPE_NODE:
-			break;
-		case URI_TYPE_ENTRY:
-			break;
-		default: throw new IllegalArgumentException( "Unsupported URI: " + uri );
-		}
-		
-		return null;
-	}
-
-	@Override
-	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 	
+	// Functions to take apart nodes and entries
+	
+	// Reads the entries under the given node
+	//
+	// @param node URI of the node 
+	public Vector<GOMEntry> entries( Uri nodeUri )
+	{
+		Vector<GOMEntry> entries = new Vector<GOMEntry>();
+		
+		return entries;
+	}
+	
+	public GOMNode entry( Uri entryUri )
+	{
+		GOMNode entry = new GOMNode();
+		
+		return entry
+		;
+	}
 }
