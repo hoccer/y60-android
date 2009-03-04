e7281d7e4eb6320adf8811f1656c1a27cbb848c6
Arne Handt <it@handtwerk.de>
2009-03-04 13:01:44 +0100

    DRYed up: added constants for GOM keywords and for the current GOM base url

---
+++ Infrastructure/src/com/artcom/y60/infrastructure/GOMConstants.java
@@ -0,0 +1,16 @@
+package com.artcom.y60.infrastructure;
+
+
+public interface GOMConstants {
+
+    // GOM keywords
+    public static final String NODE      = "node",
+                               URI       = "uri",
+                               ATTRIBUTE = "attribute",
+                               ENTRIES   = "entries",
+                               NAME      = "name",
+                               VALUE     = "value",
+                               TYPE      = "type";
+
+    public static final String GOM_BASE_URL = "http://t-gom.service.t-gallery.act";
+}
