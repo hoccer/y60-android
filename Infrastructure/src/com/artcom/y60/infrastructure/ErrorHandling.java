b819fb2291f009ace05e754f4eee8108f0e4b059
Lisa Thalheim <lisa@artcom.de>
2009-03-31 14:56:17 +0200

    more incremental changes towards error handling; stable first try

--- Infrastructure/src/com/artcom/y60/infrastructure/ErrorHandling.java
+++ Infrastructure/src/com/artcom/y60/infrastructure/ErrorHandling.java
@@ -5,22 +5,77 @@ import android.content.Intent;
 
 public class ErrorHandling {
 
-	public static final String ERROR = "...";
-	
-	public static void signal_user_error( Context context, Throwable error )
-	{
-		Intent intent = new Intent( "y60.intent.ERROR_PRESENTATION" );
-		intent.putExtra( ERROR, error);
-		context.startActivity( intent );
+	enum Category {
+
+		FILE_NOT_FOUND,
+		MALFORMED_URI,
+		UNSUPPORTED_ENCODING,
+		SAX_ERROR,
+		JSON_ERROR,
+		
+		COMPONENT_NOT_FOUND,
+		NETWORK_ERROR,
+		ILLEGAL_ARGUMENT,
+		
+		UNSPECIFIED,
 	}
 	
-	public static void signal_dev_error()
+	public static final String ID_ERROR = "error";
+	public static final String ID_LOGTAG = "logtag";
+	public static final String ID_CATEGORY = "category";
+	
+	public static void signalError( String logTag, Throwable error, Context context, Category category )
 	{
-		
+		Intent intent = new Intent("y60.intent.ERROR_PRESENTATION");
+		intent.putExtra(ID_ERROR, error);
+		intent.putExtra(ID_LOGTAG, logTag);
+		intent.putExtra(ID_CATEGORY, category);
+		context.startActivity(intent);
+	}
+
+	public static void signalFileNotFoundError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.FILE_NOT_FOUND);
+	}
+
+	public static void signalMalformedUriError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.MALFORMED_URI);
 	}
 	
-	public static void signal_admin_error()
+	public static void signalUnsupportedEncodingError(String logTag, Throwable error, Context context)
 	{
-		
+		signalError(logTag, error, context, Category.UNSUPPORTED_ENCODING);
+	}
+	
+	public static void signalSaxError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.SAX_ERROR);
+	}
+	
+	public static void signalJsonError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.JSON_ERROR);
+	}
+	
+	public static void signalComponentNotFoundError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.COMPONENT_NOT_FOUND);
+	}
+	
+	public static void signalNetworkError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.NETWORK_ERROR);
+	}
+	
+	public static void signalIllegalArgumentError(String logTag, Throwable error, Context context)
+	{
+		signalError(logTag, error, context, Category.ILLEGAL_ARGUMENT);
+	}
+	
+	public static void signalUnspecifiedError(String logTag, Throwable error, Context context)
+	{
+		// TODO this method should not be used eventually
+		signalError(logTag, error, context, Category.UNSPECIFIED);
 	}
 }
--- Infrastructure/src/com/artcom/y60/infrastructure/ErrorPresentationActivity.java
+++ Infrastructure/src/com/artcom/y60/infrastructure/ErrorPresentationActivity.java
@@ -3,6 +3,7 @@ package com.artcom.y60.infrastructure;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
@@ -13,16 +14,21 @@ public class ErrorPresentationActivity extends Activity {
 	private TableLayout mTableLayout;
 	private TextView mTextView;
 	private Button mOkButton;
+	
+	enum Mode { DEBUG, PRODUCTION }
+	
+	// TODO this will eventually need to be configurable, possibly via preferences
+	private static final Mode mMode = Mode.DEBUG;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		mTextView = new TextView( this );
-		mTableLayout = new TableLayout( this);
-		mOkButton = new Button( this );
+		mTextView = new TextView(this);
+		mTableLayout = new TableLayout(this);
+		mOkButton = new Button(this);
 		
-		mOkButton.setText( "OK" );
+		mOkButton.setText("OK");
 		mOkButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				finish();
@@ -40,9 +46,29 @@ public class ErrorPresentationActivity extends Activity {
 		super.onResume();
 		
 		Intent intent = getIntent();
-		Throwable exception = (Throwable)intent.getSerializableExtra( ErrorHandling.ERROR );
-		
-		mTextView.setText( exception.getMessage() );
+		if (mMode == Mode.DEBUG) {
+
+			String logTag = "[unknown source]";
+			if (intent.hasExtra(ErrorHandling.ID_LOGTAG)) {
+				logTag = intent.getStringExtra(ErrorHandling.ID_LOGTAG);
+			}
+			
+			String message = "(unspecified error)";
+			if (intent.hasExtra(ErrorHandling.ID_ERROR)) {
+				Throwable exception = (Throwable)intent.getSerializableExtra(ErrorHandling.ID_ERROR);
+				message = exception.getMessage();
+				
+				// Sort by class here later, for now, we just log everything in verbose mode
+				Log.v(logTag, message);
+				Log.v(logTag, exception.getStackTrace().toString());
+			}
+			
+			mTextView.setText("Oops! " + logTag + " says: " + message);
+			
+		} else if (mMode == Mode.PRODUCTION) {
+			
+			mTextView.setText(R.string.err_user_general);
+		}
 	}
 	
 }
