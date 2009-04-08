84b1f09805cb9e4f81941a142b6ea1bdd5dcfb48
Lisa Thalheim <lisa@artcom.de>
2009-04-08 10:37:37 +0200

    preparing device controller for handling search commands

--- DeviceController/src/com/artcom/y60/dc/DeviceControllerHandler.java
+++ DeviceController/src/com/artcom/y60/dc/DeviceControllerHandler.java
@@ -14,9 +14,12 @@ import org.mortbay.jetty.handler.DefaultHandler;
 
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 
+import com.artcom.y60.Actions;
+import com.artcom.y60.IntentExtraKeys;
 import com.artcom.y60.Logger;
 import com.artcom.y60.PreferencesActivity;
 
@@ -29,10 +32,10 @@ public class DeviceControllerHandler extends DefaultHandler
 	// /data/data/com.artcom.y60.dc/shared_prefs/device_controller.xml
     private static final String prefFile = "device_controller";
     
-    private Service m_Service;
+    private Service mService;
     
     public DeviceControllerHandler( Service p_Service ) {
-    	m_Service = p_Service;
+    	mService = p_Service;
     }
     
 	public void handle(String target, HttpServletRequest request,
@@ -40,7 +43,7 @@ public class DeviceControllerHandler extends DefaultHandler
 			ServletException {
 
 		
-	    SharedPreferences prefs = m_Service.getSharedPreferences( prefFile, Context.MODE_WORLD_READABLE );
+	    SharedPreferences prefs = mService.getSharedPreferences( prefFile, Context.MODE_WORLD_READABLE );
 	    String gom_location = prefs.getString( PreferencesActivity.KEY_GOM_LOCATION, "" );
 	    String device_id = prefs.getString( PreferencesActivity.KEY_DEVICE_ID, "" );
 	    String device_path = prefs.getString( PreferencesActivity.KEY_DEVICES_PATH, "" );
@@ -94,6 +97,13 @@ public class DeviceControllerHandler extends DefaultHandler
 			if (keysnvalues.length % 2 != 0 && keysnvalues.length > 0) {
 				Logger.v(LOG_TAG, "\tLonely key: ", keysnvalues[keysnvalues.length-1]);
 			}
+			
+			Intent intent = new Intent(Actions.SEARCH);
+			intent.putExtra(IntentExtraKeys.KEY_SEARCH_SENDER, kvMap.get("sender"));
+			intent.putExtra(IntentExtraKeys.KEY_SEARCH_RECEIVER, kvMap.get("receiver"));
+			intent.putExtra(IntentExtraKeys.KEY_SEARCH_ARGUMENTS, kvMap.get("arguments"));
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			mService.startActivity(intent);
 		} else {
 			respond_not_supported(request, response);
 		}
