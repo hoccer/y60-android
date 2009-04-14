6da25664aa2392983813126fbe4f3c0830350489
Arne Handt <it@handtwerk.de>
2009-04-14 15:43:25 +0200

    added TrackPad, the Y60 remote trackpad

---
+++ TrackPad/src/com/artcom/y60/trackpad/TrackPad.java
@@ -0,0 +1,222 @@
+package com.artcom.y60.trackpad;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+
+import com.artcom.y60.ErrorHandling;
+import com.artcom.y60.Logger;
+
+public class TrackPad extends Activity {
+    
+    // Constants ---------------------------------------------------------
+
+    public static final String LOG_TAG = "Trackpad";
+    
+    
+    
+    // Instance Variables ------------------------------------------------
+
+    private float mOldX = -1;
+    
+    private float mOldY = -1;
+    
+    private InetAddress mAddress;
+    
+    private int mPort;
+    
+    private DatagramSocket mSocket;
+
+    
+    
+    // Public Instance Methods -------------------------------------------
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        
+        try {
+            mAddress = InetAddress.getByName("localhost"); // TODO
+            mPort    = 4711; // TODO
+            
+        } catch (UnknownHostException x) {
+            
+            ErrorHandling.signalNetworkError(LOG_TAG, x, this);
+        }
+        setContentView(R.layout.main);
+        
+        Logger.d(LOG_TAG, "TrackPad created");
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent pEvent) {
+
+        switch (pEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN: fingerDown(pEvent); return true;
+            case MotionEvent.ACTION_UP:   fingerUp(pEvent);   return true;
+            case MotionEvent.ACTION_MOVE: move(pEvent);       return true;
+        }
+        
+        return super.onTouchEvent(pEvent);
+    }
+
+    @Override
+    public boolean onKeyDown(int pKeyCode, KeyEvent pEvent) {
+        
+        switch (pKeyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:    move(0, -1); return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:  move(0,  1); return true;
+            case KeyEvent.KEYCODE_DPAD_LEFT:  move(-1, 0); return true;
+            case KeyEvent.KEYCODE_DPAD_RIGHT: move(1,  0); return true;
+        }
+        
+        return super.onKeyDown(pKeyCode, pEvent);
+    }
+
+    
+    
+    // Protected Instance Methods ----------------------------------------
+
+    @Override
+    protected void onPause() {
+        
+        disconnectFromDisplay();
+        
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+
+    // Private Instance Methods ------------------------------------------
+
+    private void fingerDown(MotionEvent pEvent) {
+        
+        mOldX = pEvent.getX();
+        mOldY = pEvent.getY();
+    }
+    
+    private void fingerUp(MotionEvent pEvent) {
+        
+        mOldX = -1;
+        mOldY = -1;
+    }
+    
+    private void move(MotionEvent pEvent) {
+        
+        if (mOldX > -1 && mOldY > -1) {
+            
+            // origin is top left corner
+            float dx = pEvent.getX() - mOldX;
+            float dy = pEvent.getY() - mOldY;
+            mOldX = pEvent.getX();
+            mOldY = pEvent.getY();
+            
+            move(dx, dy);
+            
+        } else {
+            
+            Logger.v(LOG_TAG, "ignoring unexpected move event, x = ", pEvent.getX(), ", y = ", pEvent.getY());
+        }
+    }
+    
+    
+    private void move(float pX, float pY) {
+        
+        Logger.v(LOG_TAG, "move, dx = ", pX, ", dy = ", pY);
+        
+        if (pX > 0) {
+            Logger.v(LOG_TAG, "moving right");
+        } else if (pX < 0) {
+            Logger.v(LOG_TAG, "moving left");
+        } else {
+            Logger.v(LOG_TAG, "no horizontal movement");
+        }
+        
+        if (pY > 0) {
+            Logger.v(LOG_TAG, "moving down");
+        } else if (pY < 0) {
+            Logger.v(LOG_TAG, "moving up");
+        } else {
+            Logger.v(LOG_TAG, "no vertical movement");
+        }
+        
+        try {
+            sendMoveEvent(pX, pY);
+            
+        } catch (SocketException x) {
+            
+            ErrorHandling.signalNetworkError(LOG_TAG, x, this);
+            
+        } catch (IOException iox) {
+            
+            ErrorHandling.signalIOError(LOG_TAG, iox, this);
+        }
+    }
+    
+    
+    private void connectToDisplay() throws SocketException {
+        
+        if (isConnected()) {
+            Logger.w(LOG_TAG, "already connected - ignoring");
+            return;
+        }
+        
+        mSocket = new DatagramSocket();
+    }
+    
+    
+    private void disconnectFromDisplay() {
+        
+        if (!isConnected()) {
+            Logger.w(LOG_TAG, "not connected - ignoring");
+            return;
+        }
+        
+        mSocket.disconnect();
+        mSocket = null;
+    }
+    
+    
+    private void sendMoveEvent(float pX, float pY) throws IOException, SocketException {
+        
+        if (!isConnected()) {
+            
+            connectToDisplay();
+        }
+            
+        byte[]         payload = new byte[]{ deltaToByte(pX), deltaToByte(pY) };
+        DatagramPacket packet  = new DatagramPacket(payload, payload.length, mAddress, mPort);
+        
+        Logger.d(LOG_TAG, "sending bytes ", payload[0], ", ", payload[1], " as UDP datagram");
+        
+        mSocket.send(packet);
+    }
+    
+    
+    private byte deltaToByte(float pDelta) {
+        
+        // should suffice
+        return (byte)pDelta;
+    }
+    
+    
+    private boolean isConnected() {
+        
+        return (mSocket != null) && (mSocket.isConnected());
+    }
+}
---
+++ TrackPad/tests/com/artcom/y60/trackpad/TrackPadInstrumentationTest.java
@@ -0,0 +1,122 @@
+package com.artcom.y60.trackpad;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import android.view.KeyEvent;
+
+import com.artcom.y60.Logger;
+import com.artcom.y60.Y60ActivityInstrumentationTest;
+
+public class TrackPadInstrumentationTest extends Y60ActivityInstrumentationTest<TrackPad> {
+
+    // Constructors ------------------------------------------------------
+
+    public TrackPadInstrumentationTest() {
+        
+        super("com.artcom.y60.trackpad", TrackPad.class);
+    }
+    
+    
+    
+    // Public Instance Methods -------------------------------------------
+
+    @Override
+    public void setUp() throws Exception {
+        
+        super.setUp();
+    }
+    
+    
+    @Override
+    public void tearDown() throws Exception {
+        
+        getActivity().finish();
+        super.tearDown();
+    }
+    
+    
+    public void testTrackball() throws Exception {
+        
+        testTrackballMove(KeyEvent.KEYCODE_DPAD_UP,     0, -1);
+        testTrackballMove(KeyEvent.KEYCODE_DPAD_DOWN,   0,  1);
+        testTrackballMove(KeyEvent.KEYCODE_DPAD_LEFT,  -1,  0);
+        testTrackballMove(KeyEvent.KEYCODE_DPAD_RIGHT,  1,  0);
+    }
+
+    
+    
+    // Private Instance Methods ------------------------------------------
+
+    private void testTrackballMove(int pKeyCode, int pX, int pY) throws Exception {
+        
+        Logger.v(tag(), "testTrackballMove");
+        MovementServerThread server = new MovementServerThread();
+        server.start();
+        pressKey(pKeyCode, 100);
+        
+        try {
+            
+            Thread.sleep(50);
+            
+        } catch (InterruptedException x) {
+            
+            // kthxbye
+        }
+        
+        if (server.error != null) {
+            
+            throw new RuntimeException(server.error);
+        }
+        
+        if (server.x == null || server.y == null) {
+            
+            fail("Didn't receive a datagram with movement information!");
+        }
+        
+        assertEquals("x delta mismatch", pX, server.x.byteValue());
+        assertEquals("y delta mismatch", pY, server.y.byteValue());
+    }
+
+    
+    
+    // Inner Classes -----------------------------------------------------
+
+    class MovementServerThread extends Thread {
+
+        private DatagramSocket mSocket = null;
+        
+        Byte x = null;
+        
+        Byte y = null;
+        
+        Throwable error;
+
+        public MovementServerThread() throws IOException {
+            
+            super("MovementServerThread");
+            mSocket = new DatagramSocket(4711);
+        }
+
+        public void run() {
+
+            try {
+                byte[] buffer = new byte[2];
+
+                // receive request
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                mSocket.receive(packet);
+                
+                x = buffer[0];
+                y = buffer[1];
+
+            } catch (IOException e) {
+                
+                error = e;
+            }
+            mSocket.close();
+        }
+    }
+
+}
