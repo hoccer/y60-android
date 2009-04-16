0763b9d5c040483ef6d601a5fb98478676b06abb
Fabian Bützow <fabian@artcom.de>
2009-04-16 10:11:17 +0200

    differ between different command targets

--- DeviceController/src/com/artcom/y60/dc/DeviceControllerHandler.java
+++ DeviceController/src/com/artcom/y60/dc/DeviceControllerHandler.java
@@ -22,118 +22,132 @@ import com.artcom.y60.Actions;
 import com.artcom.y60.IntentExtraKeys;
 import com.artcom.y60.Logger;
 import com.artcom.y60.PreferencesActivity;
+import com.artcom.y60.ErrorHandling;
 
+public class DeviceControllerHandler extends DefaultHandler {
+    private static final String LOG_TAG = "DeviceControllerHandler";
 
-public class DeviceControllerHandler extends DefaultHandler
-{
-	private static final String LOG_TAG = "DeviceControllerHandler";
-    
-	// name of the preferences file. android stores this under
-	// /data/data/com.artcom.y60.dc/shared_prefs/device_controller.xml
+    // name of the preferences file. android stores this under
+    // /data/data/com.artcom.y60.dc/shared_prefs/device_controller.xml
     private static final String prefFile = "device_controller";
-    
+
     private Service mService;
-    
-    public DeviceControllerHandler( Service p_Service ) {
-    	mService = p_Service;
+
+    public DeviceControllerHandler(Service p_Service) {
+        mService = p_Service;
     }
-    
-	public void handle(String target, HttpServletRequest request,
-			HttpServletResponse response, int dispatch) throws IOException,
-			ServletException {
 
-		
-	    SharedPreferences prefs = mService.getSharedPreferences( prefFile, Context.MODE_WORLD_READABLE );
-	    String gom_location = prefs.getString( PreferencesActivity.KEY_GOM_LOCATION, "" );
-	    String device_id = prefs.getString( PreferencesActivity.KEY_DEVICE_ID, "" );
-	    String device_path = prefs.getString( PreferencesActivity.KEY_DEVICES_PATH, "" );
+    public void handle(String target, HttpServletRequest request, HttpServletResponse response,
+            int dispatch) throws IOException, ServletException {
 
-		Logger.v(LOG_TAG , "Target: ", target);
-		String method = request.getMethod();
-		Logger.v(LOG_TAG, "Method: ", method);
+        SharedPreferences prefs = mService.getSharedPreferences(prefFile,
+                Context.MODE_WORLD_READABLE);
+        String gom_location = prefs.getString(PreferencesActivity.KEY_GOM_LOCATION, "");
+        String device_id = prefs.getString(PreferencesActivity.KEY_DEVICE_ID, "");
+        String device_path = prefs.getString(PreferencesActivity.KEY_DEVICES_PATH, "");
 
-		String path = request.getPathInfo();
-		String location = "";
+        Logger.v(LOG_TAG, "Target: ", target);
+        String method = request.getMethod();
+        Logger.v(LOG_TAG, "Method: ", method);
 
-		String self = device_path + "/" + device_id;
+        String path = request.getPathInfo();
+        String location = "";
 
-		if (path.startsWith( "/proc" )) {
-			// Requests directed at the DC's own resources.
-		    Logger.v(LOG_TAG, "Handling /proc request");
-			ProcHandler procHandler = new ProcHandler();
-			procHandler.handle(target, request, response, dispatch);
-			return;
-		}
-		
-		// All other GET requests are redirected to the GOM
-		if (method.equals("GET") || method.equals("HEAD")) {
-			
-			if (path == null) {
-				path = "";
-			}
-        
-			if (path.startsWith("/self")) {
-				location = gom_location + self + path.replaceFirst("/self", ""); 
-			} else {
-				location = gom_location + path;
-			}	
+        String self = device_path + "/" + device_id;
 
-			response.setStatus(HttpServletResponse.SC_SEE_OTHER);
-			response.addHeader("Location", location);
+        if (path.startsWith("/proc")) {
+            // Requests directed at the DC's own resources.
+            Logger.v(LOG_TAG, "Handling /proc request");
+            ProcHandler procHandler = new ProcHandler();
+            procHandler.handle(target, request, response, dispatch);
+            return;
+        }
+
+        // All other GET requests are redirected to the GOM
+        if (method.equals("GET") || method.equals("HEAD")) {
+
+            if (path == null) {
+                path = "";
+            }
+
+            if (path.startsWith("/self")) {
+                location = gom_location + self + path.replaceFirst("/self", "");
+            } else {
+                location = gom_location + path;
+            }
+
+            response.setStatus(HttpServletResponse.SC_SEE_OTHER);
+            response.addHeader("Location", location);
             response.setContentType("text/plain");
             response.setContentLength(0);
-		
-		} else if (method.equals("POST")) {
 
-			String parameters = "";
-			if (request.getContentLength() > 0) { // arguments are supplied in request body
-				BufferedReader reader = request.getReader();
-				parameters = reader.readLine();
-			} else { // arguments are supplied in request string
-				parameters = request.getQueryString();
-			}
-		
-			MultiMap kvMap = new MultiMap();
-			UrlEncoded.decodeUtf8To(parameters.getBytes(), 0, parameters.length(), kvMap);
-			
-//			String[] keysnvalues = parameters.split("[=&]");
-//			Map<String,String> kvMap = new HashMap<String,String>();
-//			Logger.v(LOG_TAG, keysnvalues.length, " elements");
-//			for (int i = 0; i*2+1 < keysnvalues.length; i++) {
-//				UrlEncoded encodedKey = new UrlEncoded(keysnvalues[i*2]).de;
-//				kvMap.put(keysnvalues[i*2], keysnvalues[i*2+1]);
-//				Logger.v(LOG_TAG, "\t", keysnvalues[i*2], " = ", keysnvalues[i*2+1]);
-//			}
-//			if (keysnvalues.length % 2 != 0 && keysnvalues.length > 0) {
-//				Logger.v(LOG_TAG, "\tLonely key: ", keysnvalues[keysnvalues.length-1]);
-//			}
-			
-			Logger.v(LOG_TAG, "sender = " + (String)kvMap.get("sender") +
-					", receiver = " + (String)kvMap.get("receiver") +
-					", arguments = " + (String)kvMap.get("arguments"));
-			
-			Intent intent = new Intent(Actions.SEARCH);
-			intent.putExtra(IntentExtraKeys.KEY_SEARCH_SENDER, (String)kvMap.get("sender"));
-			intent.putExtra(IntentExtraKeys.KEY_SEARCH_RECEIVER, (String)kvMap.get("receiver"));
-			intent.putExtra(IntentExtraKeys.KEY_SEARCH_ARGUMENTS, (String)kvMap.get("arguments"));
-			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			mService.startActivity(intent);
-			
-		} else {
-			respond_not_supported(request, response);
-		}
-		
-		Request base_request = (request instanceof Request) ? (Request)request:HttpConnection.getCurrentConnection().getRequest();
-		base_request.setHandled(true);
-	}
-	
-    private void respond_not_supported( HttpServletRequest request,
-            HttpServletResponse response )
-        throws ServletException, IOException
-    {
+        } else if (method.equals("POST")) {
+
+            String parameters = "";
+            if (request.getContentLength() > 0) { // arguments are supplied in
+                // request body
+                BufferedReader reader = request.getReader();
+                parameters = reader.readLine();
+            } else { // arguments are supplied in request string
+                parameters = request.getQueryString();
+            }
+
+            MultiMap kvMap = new MultiMap();
+            UrlEncoded.decodeUtf8To(parameters.getBytes(), 0, parameters.length(), kvMap);
+
+            // String[] keysnvalues = parameters.split("[=&]");
+            // Map<String,String> kvMap = new HashMap<String,String>();
+            // Logger.v(LOG_TAG, keysnvalues.length, " elements");
+            // for (int i = 0; i*2+1 < keysnvalues.length; i++) {
+            // UrlEncoded encodedKey = new UrlEncoded(keysnvalues[i*2]).de;
+            // kvMap.put(keysnvalues[i*2], keysnvalues[i*2+1]);
+            // Logger.v(LOG_TAG, "\t", keysnvalues[i*2], " = ",
+            // keysnvalues[i*2+1]);
+            // }
+            // if (keysnvalues.length % 2 != 0 && keysnvalues.length > 0) {
+            // Logger.v(LOG_TAG, "\tLonely key: ",
+            // keysnvalues[keysnvalues.length-1]);
+            // }
+
+            Logger.v(LOG_TAG, "target = " + (String) kvMap.get("target") + ", sender = "
+                    + (String) kvMap.get("sender") + ", receiver = "
+                    + (String) kvMap.get("receiver") + ", arguments = "
+                    + (String) kvMap.get("arguments"));
+
+            Intent intent;
+            if ((kvMap.get("target")).equals("search")) {
+                intent = new Intent(Actions.SEARCH);
+            } else if ((kvMap.get("target")).equals("voice_control")) {
+                intent = new Intent(Actions.SEARCH);
+            } else {
+                ErrorHandling.signalComponentNotFoundError(LOG_TAG, new Exception(
+                        "illegal RCA target: " + (kvMap.get("target")) + "from: "
+                                + (kvMap.get("sender"))), mService);
+                return;
+            }
+
+            intent.putExtra(IntentExtraKeys.KEY_SEARCH_TARGET, (String) kvMap.get("target"));
+            intent.putExtra(IntentExtraKeys.KEY_SEARCH_SENDER, (String) kvMap.get("sender"));
+            intent.putExtra(IntentExtraKeys.KEY_SEARCH_RECEIVER, (String) kvMap.get("receiver"));
+            intent.putExtra(IntentExtraKeys.KEY_SEARCH_ARGUMENTS, (String) kvMap.get("arguments"));
+
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mService.startActivity(intent);
+
+        } else {
+            respond_not_supported(request, response);
+        }
+
+        Request base_request = (request instanceof Request) ? (Request) request : HttpConnection
+                .getCurrentConnection().getRequest();
+        base_request.setHandled(true);
+    }
+
+    private void respond_not_supported(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/plain");
-        response.setStatus( HttpServletResponse.SC_NOT_IMPLEMENTED );
+        response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
         response.setContentLength(0);
     }
-	
+
 }
