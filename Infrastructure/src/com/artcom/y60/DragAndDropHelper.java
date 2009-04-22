cef3bb697a28b488cf0eb5614abf9a80f1706c5c
Arne Handt <it@handtwerk.de>
2009-04-22 18:31:55 +0200

    drag and drop implemented for absolute layouts, still some problems

--- Infrastructure/src/com/artcom/y60/DragAndDropHelper.java
+++ Infrastructure/src/com/artcom/y60/DragAndDropHelper.java
@@ -1,11 +1,9 @@
 package com.artcom.y60;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.View.OnTouchListener;
 import android.view.animation.Animation;
@@ -18,69 +16,105 @@ import android.widget.AbsoluteLayout.LayoutParams;
 
 public class DragAndDropHelper implements OnTouchListener {
     
+    // Constants ---------------------------------------------------------
+
     public static final String LOG_TAG = "DragAndDropHelper";
     
-    private static final float SCALE_FACTOR = 0.3f;
+    private static final float SCALE_FACTOR = 0.5f;
     
     private static final int ANIMATION_DURATION = 1000;
     
-    private ViewGroup mLayout;
-    private View mSourceView;
-    private Activity mActivity;
-    private View mThumbView;
-    private GestureDetector mGest;
-    private AbsoluteLayout mDragLayout;
+    // Instance Variables ------------------------------------------------
     
-    public static DragAndDropHelper enableDragAndDrop(View pView, ViewGroup pLayout, Activity pActivity){
+    /** The Layout in which the dragging takes place */
+    private AbsoluteLayout mLayout;
+    
+    /** The View to be dragged around */
+    private View mSourceView;
+    
+    /** The foreground activity that displayed the draggable view */
+    private Activity mActivity;
+    
+    /** A thumbnailed screenshot of the draggable view */
+    private View mThumbView;
+    
+    /** Used for detecting the long press */
+    private GestureDetector mGest;
+    
+    // Static Methods ----------------------------------------------------
+    
+    /**
+     * Makes the given draggable by longpressing it.
+     */
+    public static DragAndDropHelper enableDragAndDrop(View pView, AbsoluteLayout pLayout, Activity pActivity){
         
         DragAndDropHelper helper = new DragAndDropHelper(pView, pLayout, pActivity);
-        pView.setOnTouchListener(helper);
         return helper;
     }
     
-    
-    public DragAndDropHelper(View pView, ViewGroup pLayout, Activity pActivity){
+    // Constructors ------------------------------------------------------
+
+    public DragAndDropHelper(View pView, AbsoluteLayout pLayout, Activity pActivity){
         
         mLayout = pLayout;
+        mLayout.setOnTouchListener(this);
+        
         mSourceView = pView;
+        mSourceView.setOnTouchListener(this);
+        
         mActivity = pActivity;
         mGest = new GestureDetector(new ShareGestureListener());
     }
     
+    // Public Instance Methods -------------------------------------------
 
     @Override
     public boolean onTouch(View pV, MotionEvent pEvent) {
     
+        Logger.d(LOG_TAG, "onTouch ", pV, " ", pEvent.getAction());
+        
         if (mThumbView == null) {
-             
+            
             mThumbView = GraphicsHelper.scaleView(mSourceView, SCALE_FACTOR, mActivity);
-            //mThumbView.setVisibility(View.INVISIBLE);
-                       
-            mDragLayout= new AbsoluteLayout(mActivity);
-            mDragLayout.addView(mThumbView);
-            mDragLayout.setOnTouchListener(this);
+            mThumbView.setVisibility(View.INVISIBLE);
             mThumbView.setOnTouchListener(this);
+            mLayout.addView(mThumbView);
         }
         
-        switch (pEvent.getAction()) {
-            case (MotionEvent.ACTION_UP):
-                Logger.d(LOG_TAG, "and up!!!");
-                //mThumbView.setVisibility(View.INVISIBLE);
-//                mSourceView.setVisibility(View.VISIBLE);
-                mActivity.setContentView(mLayout);
-                mLayout.invalidate();
-                break;
-                
-            case (MotionEvent.ACTION_MOVE):
-                if (mThumbView.getVisibility() == View.VISIBLE) {
-                    mThumbView.setLayoutParams(positionForDragging(pEvent));
-                }
-                break;
+        if (isCurrentlyDragging()) {
+            
+            switch (pEvent.getAction()) {
+                case (MotionEvent.ACTION_UP):
+                    endDragging();
+                    break;
+                case (MotionEvent.ACTION_MOVE):
+                    drag(pEvent);
+                    break;
+            }
         }
         
-        return mGest.onTouchEvent(pEvent);
+        if (pV == mSourceView || isCurrentlyDragging()) {
+            mGest.onTouchEvent(pEvent);
+        }
+        
+        return true;
     }
+
+    // Private Instance Methods ------------------------------------------
     
+    private void drag(MotionEvent pEvent) {
+        
+        mThumbView.setLayoutParams(positionForDragging(pEvent));
+    }
+
+    private void endDragging() {
+        
+        Logger.d(LOG_TAG, "and up!!!");
+        mThumbView.setVisibility(View.INVISIBLE);
+        mSourceView.setVisibility(View.VISIBLE);
+        mLayout.invalidate();
+    }
+
     private LayoutParams positionForDragging(MotionEvent pEvent) {
         
         int x = (int)pEvent.getX()-mThumbView.getWidth()/2;
@@ -92,30 +126,18 @@ public class DragAndDropHelper implements OnTouchListener {
                                 y);
     }
     
+    private boolean isCurrentlyDragging() {
+        return mThumbView.getVisibility() == View.VISIBLE;
+    }
+    
+    // Inner Classes -----------------------------------------------------
     
     class ShareGestureListener implements OnGestureListener {
-
-        @Override
-        public boolean onDown(MotionEvent pE) {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent pE1, MotionEvent pE2, float pVelocityX,
-                float pVelocityY) {
-            // TODO Auto-generated method stub
-            return false;
-        }
 
         @Override
         public void onLongPress(MotionEvent pE) {
             
             Logger.v(LOG_TAG, "LOOOOOOOOOOOOOOOOOOOOOOOONG PRESS");
-            
-            
-            
-            //((Activity)mContext).getWindow().getWindowManager().
             
             int x = (int)pE.getX();//-mDrawable.getMinimumWidth()/2;
             int y = (int)pE.getY()-10;//-mDrawable.getMinimumHeight()*2;
@@ -126,7 +148,6 @@ public class DragAndDropHelper implements OnTouchListener {
                     y-(int)(mSourceView.getHeight()*SCALE_FACTOR/2));
 
             mThumbView.setLayoutParams(params);
-            
             
             TranslateAnimation translate = new TranslateAnimation(0, x-mSourceView.getWidth()/2,
                     0, y-mSourceView.getHeight()/2);
@@ -144,48 +165,27 @@ public class DragAndDropHelper implements OnTouchListener {
             anims.setAnimationListener(new ThumbnailAnimationListener());
             
             mSourceView.startAnimation(anims);
-             
         }
 
-        @Override
-        public boolean onScroll(MotionEvent pE1, MotionEvent pE2, float pDistanceX,
-                float pDistanceY) {
-            // TODO Auto-generated method stub
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent pE) {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent pE) {
-            // TODO Auto-generated method stub
-            return false;
-        }
-        
+        public boolean onDown(MotionEvent pE) { return false; }
+        public boolean onFling(MotionEvent pE1, MotionEvent pE2, float pVelocityX, float pVelocityY) { return false; }
+        public boolean onScroll(MotionEvent pE1, MotionEvent pE2, float pDistanceX, float pDistanceY) { return false; }
+        public void onShowPress(MotionEvent pE) {}
+        public boolean onSingleTapUp(MotionEvent pE) { return false; }
     }
 
     class ThumbnailAnimationListener implements Animation.AnimationListener {
         
         public void onAnimationEnd(Animation animation) {
+            
             Logger.d(LOG_TAG, "animation end --------------------------------");
-            //mSourceView.setVisibility(View.INVISIBLE);
-            //mThumbView.setVisibility(View.VISIBLE);
-            mActivity.setContentView(mDragLayout);
-//            tab.setImageResource(R.drawable.up);
-//            contents.setVisibility(View.GONE);
+//            mLayout.getChildAt(0).setVisibility(View.INVISIBLE);
+            mSourceView.setVisibility(View.INVISIBLE);
+            mThumbView.setVisibility(View.VISIBLE);
+            mLayout.invalidate();
         }
 
-        public void onAnimationRepeat(Animation animation) {
-            // not needed
-        }
-
-        public void onAnimationStart(Animation animation) {
-            // not needed
-        }
+        public void onAnimationRepeat(Animation animation) {}
+        public void onAnimationStart(Animation animation) {}
     }
-
 }
