99d2ff161ccb7108e6a85305e3ab96a52c8e5254
Rodja Trappe <rodja@artcom.de>
2009-04-24 14:34:37 +0200

    fixed intending

--- Infrastructure/tests/com/artcom/y60/TestError.java
+++ Infrastructure/tests/com/artcom/y60/TestError.java
@@ -5,25 +5,24 @@ import android.content.IntentFilter;
 import android.test.ActivityInstrumentationTestCase;
 
 public class TestError extends ActivityInstrumentationTestCase<ErrorPresentationActivity> {
-	
-	public TestError() {
-	    super("com.artcom.y60", ErrorPresentationActivity.class);
-		
-	}
 
-	public void testUserError()
-	{
-	    assertNotNull(getActivity());
-	    
-		IntentFilter filter = new IntentFilter("y60.intent.ERROR_PRESENTATION");
-		ActivityMonitor monitor = new ActivityMonitor( filter, null, true );
-		getInstrumentation().addMonitor(monitor);
-		
-		Exception e = new Exception( "Don't panic" );
-		ErrorHandling.signalError( this.getClass().getSimpleName(),
-				e, getActivity(), ErrorHandling.Category.UNSPECIFIED );
-		
-		assertEquals("no activity launced", 1, monitor.getHits());
-	}
-	
+    public TestError() {
+        super("com.artcom.y60", ErrorPresentationActivity.class);
+
+    }
+
+    public void testUserError() {
+        assertNotNull(getActivity());
+
+        IntentFilter filter = new IntentFilter("y60.intent.ERROR_PRESENTATION");
+        ActivityMonitor monitor = new ActivityMonitor(filter, null, true);
+        getInstrumentation().addMonitor(monitor);
+
+        Exception e = new Exception("Don't panic");
+        ErrorHandling.signalError(this.getClass().getSimpleName(), e, getActivity(),
+                ErrorHandling.Category.UNSPECIFIED);
+
+        assertEquals("no activity launced", 1, monitor.getHits());
+    }
+
 }
