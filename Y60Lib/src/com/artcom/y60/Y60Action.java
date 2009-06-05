0434fcfed7a07e9508a5f1861c932628acd98c7c
Arne Handt <it@handtwerk.de>
2009-06-05 17:47:07 +0200

    removed action that was never used

--- Y60Lib/src/com/artcom/y60/Y60Action.java
+++ Y60Lib/src/com/artcom/y60/Y60Action.java
@@ -3,8 +3,6 @@ package com.artcom.y60;
 
 public abstract class Y60Action {
 
-	public static final String APPLICATION_ADMIN = "y60.intent.APPLICATION_ADMIN";
-	
 	public static final String SEARCH = "y60.intent.SEARCH";
 	public static final String SEARCH_BC = "y60.intent.SEARCH_BC";
     
