43af359aa1e4a2cbc94c4f5f04b8ce6a6ec246a7
Rodja Trappe <rodja@artcom.de>
2009-07-28 11:57:04 +0200

    added project priority for tests: failing projects get tested first the next time

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require "yaml"
+
 $:.unshift File.join(File.dirname(__FILE__), '..', 'scripts')
 require 'project'
 require 'java_project'
@@ -7,14 +9,39 @@ require 'android_project'
 
 def main pj_names
 
-  projects = [] #Project.load_in_dependency_order("TgHomeScreen")
-
-  Dir["#{Dir.getwd}/*/.project"].each { |dir|
-    pj_name = File.basename(File.dirname dir)
-    projects.push(Project.find_or_create pj_name, Dir.getwd)
+  last_project_sorting_file = "/tmp/project_sorting.yaml"
+  if File.exists? last_project_sorting_file then
+    last_project_sorting = YAML::load_file(last_project_sorting_file);
+  else
+    last_project_sorting = []
+  end
+  
+  project_paths = Dir["#{Dir.getwd}/*/.project"]
+  puts "project paths #{project_paths}"
+  projects = []
+  
+  
+  last_project_sorting.each { |name|
+    
+    index = project_paths.index{|p| p.include? name}
+    puts "#################### #{name} #{index} ----"
+    if index != nil then
+      puts "adding #{name} to projects (was at index #{index})"
+      projects.push(Project.find_or_create name, Dir.getwd)
+      project_paths.delete_at index
+    else
+      puts "#{name} is not included in list of project_path's"
+    end
+  }
+  
+  
+  project_paths.each { |dir|
+    name = File.basename(File.dirname dir)
+    projects.push(Project.find_or_create name, Dir.getwd)
   }
 
   puts "testing #{projects.map {|p| p.name}.join(' ')}"
+  
   
   failing_projects = []
   tests_run = tests_failed = tests_with_exception = broken_instrumentations = 0
@@ -22,14 +49,15 @@ def main pj_names
  
   projects = projects.select { |p| p.respond_to? :test }
 
-    
-    success = projects.inject do |yet, project|
+  success = projects.inject do |yet, project|
     starttime = Time.new
 
     puts "#{Time.now.to_s}--- running test for project #{project.name}"
     test_result = project.test
     
-    if !test_result[:was_succsessful] then failing_projects.push project.name end
+    if !test_result[:was_succsessful] then 
+      failing_projects.push project.name
+    end
     
     if test_result.has_key? :tests_run then 
       tests_run += test_result[:tests_run]
@@ -66,6 +94,9 @@ def main pj_names
     exit 0
   else
     puts "There where test failures in #{failing_projects.join ', '}"
+    last_project_sorting = failing_projects + (last_project_sorting - failing_projects)
+    
+    File.open(last_project_sorting_file, 'w') {|f| f.write(last_project_sorting.to_yaml) }
     exit 1
   end
   
