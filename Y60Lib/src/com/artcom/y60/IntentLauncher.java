611ced02cf393c262e463d298020c841bf90afdc
Rodja Trappe <rodja@artcom.de>
2009-08-07 17:00:36 +0200

    flow for newtasks from homescreen

--- Y60Lib/src/com/artcom/y60/IntentLauncher.java
+++ Y60Lib/src/com/artcom/y60/IntentLauncher.java
@@ -6,46 +6,43 @@ import android.content.Intent;
 public class IntentLauncher extends SlotLauncher {
 
     private static final String LOG_TAG = IntentLauncher.class.getName();
-    
+
     // Instance Variables ------------------------------------------------
 
-    private Intent mIntent;
-    
-    
-    
+    private Intent              mIntent;
+
     // Constructors ------------------------------------------------------
 
     public IntentLauncher(Intent pIntent) {
-        
+
         mIntent = pIntent;
     }
-    
+
     public IntentLauncher(String pActivityClass) {
-        
-        int    dotPos = pActivityClass.lastIndexOf(".");
-        String pkg    = pActivityClass.substring(0, dotPos);
+
+        int dotPos = pActivityClass.lastIndexOf(".");
+        String pkg = pActivityClass.substring(0, dotPos);
         ComponentName compName = new ComponentName(pkg, pActivityClass);
         mIntent = new Intent();
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mIntent.setComponent(compName);
     }
-    
-    
-    
+
     // Public Instance Methods -------------------------------------------
-    
+
     @Override
     public void launch() {
         Logger.v(LOG_TAG, "launch intent slot launcher");
         getContext().startActivity(mIntent);
     }
-    
+
     public String toString() {
-        
+
         return mIntent.toString();
     }
-    
+
     public String getActivityClass() {
-        
+
         return mIntent.getComponent().getClassName();
     }
 }
