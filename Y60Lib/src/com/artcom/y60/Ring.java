2559c5ed42c835fab8d43e232d977806ec7a79e9
Robert Palmer <robert.palmer@artcom.de>
2009-08-18 15:35:39 +0200

    new ring class

---
+++ Y60Lib/src/com/artcom/y60/Ring.java
@@ -0,0 +1,39 @@
+package com.artcom.y60;
+
+import java.util.ArrayList;
+
+public abstract class Ring<T> {
+
+    private ArrayList<T> mRingContent = new ArrayList<T>();
+    private int          mCurrent     = 0;                  ;
+
+    public void add(T pNewMember) {
+        mRingContent.add(pNewMember);
+    }
+
+    public void setActive(int pIndex) {
+        pIndex = calculateRealIndex(pIndex);
+        onActiveChange(mRingContent.get(mCurrent), mRingContent.get(pIndex));
+
+        mCurrent = pIndex;
+    }
+
+    public void setNext() {
+        setActive(mCurrent + 1);
+    }
+
+    public void setPrevious() {
+        setActive(mCurrent - 1);
+    }
+
+    protected abstract void onActiveChange(T pOldActiveMember, T pNextActiveMember);
+
+    private int calculateRealIndex(int pIndex) {
+        pIndex = pIndex % mRingContent.size();
+
+        if (pIndex < 0)
+            pIndex = pIndex + mRingContent.size();
+
+        return pIndex;
+    }
+}
