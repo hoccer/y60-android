cba83e20cf9505db43b3c66a52045ec16ada06d5
Rodja Trappe <rodja@artcom.de>
2009-08-30 17:13:14 +0200

    now the logger lines will also be removed

--- scripts/removeLoggerV.sh
+++ scripts/removeLoggerV.sh
@@ -3,5 +3,8 @@
 FILE=$1
 
 cat $FILE | \
-sed -n '1h;1!H;${;g;s/Logger\.v([^\;]*)\;//g;p;}' #> $FILE
+sed -n '1h;1!H;${;g;s/Logger\.v([^\;]*)\;/### removed logger ###/g;p;}' > /tmp/removeLogger.tmp #$FILE
+
+cat /tmp/removeLogger.tmp | \
+sed -e '/### removed logger ###/d' > $FILE
 
