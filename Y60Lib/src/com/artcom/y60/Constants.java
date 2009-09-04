e68c5b38505ad66f5b1770b3393cc4800eaab2f6
katjae <katjae@artcom.de>
2009-09-04 15:36:32 +0200

    more constss

--- Y60Lib/src/com/artcom/y60/Constants.java
+++ Y60Lib/src/com/artcom/y60/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
         public static final String       OBSERVER_BASE_PATH    = "/gom/observer";
 
         public static final IntentFilter GNP_INTENT_FILTER;
+        public static final String       QUESTION_PATH         = "/questions";
 
         static {
             DeviceConfiguration config = DeviceConfiguration.load();
