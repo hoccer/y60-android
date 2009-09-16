7e987e85847d132fc85324ce7219cf2de3606f3c
katjae <katjae@artcom.de>
2009-09-16 14:32:58 +0200

    even more intent keys

--- Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
+++ Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
@@ -1,6 +1,7 @@
 package com.artcom.y60;
 
 public class IntentExtraKeys {
+    public static final String KEY_CALLING_APP              = "calling_app";
     public static final String KEY_SEARCH_TARGET            = "target";
     public static final String KEY_SEARCH_SENDER            = "sender";
     public static final String KEY_SEARCH_RECEIVER          = "receiver";
