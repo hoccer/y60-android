b3e244c0f53748e37796c4f23a52ab0e95593834
katjae <katjae@artcom.de>
2009-10-07 11:21:18 +0200

    new intent keys

--- Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
+++ Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
@@ -31,6 +31,7 @@ public class IntentExtraKeys {
     public static final String POSSESSION_URI           = "possession_uri";
     public static final String IMAGE_URI                = "image_uri";
     public static final String MEDIA_URI                = "media_uri";
+    public static final String ASSET_URI                = "asset_uri";
     public static final String MEDIA_NAME               = "media_name";
     public static final String MEDIA_TYPE               = "media_type";
 
