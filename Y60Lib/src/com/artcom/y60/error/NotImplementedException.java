5362bc51de4b2b0b11041e0d23c5cfca3ab0e09d
Rodja Trappe <rodja@artcom.de>
2009-10-15 12:52:49 +0200

    new exception to show that something is not jet implemented

---
+++ Y60Lib/src/com/artcom/y60/error/NotImplementedException.java
@@ -0,0 +1,14 @@
+package com.artcom.y60.error;
+
+public class NotImplementedException extends Exception {
+
+    public NotImplementedException(String pMessage) {
+        super(pMessage);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 2189241074977722062L;
+
+}
