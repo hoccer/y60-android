b5fb97cff000d7386688b98004b18ead88adca71
katjae <katjae@artcom.de>
2009-10-20 16:04:38 +0200

    small chhnages

--- Y60Lib/src/com/artcom/y60/IoHelper.java
+++ Y60Lib/src/com/artcom/y60/IoHelper.java
@@ -12,6 +12,8 @@ public class IoHelper {
 
     public static String encodeUrlBlanks(String pString) {
         pString = pString.replaceAll(" ", "%20");
+        // pString = pString.replaceAll("ü", "%FC"); //not sure if this is
+        // needed
         return pString;
     }
 
