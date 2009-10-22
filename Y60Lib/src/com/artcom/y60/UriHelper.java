d644fd4484be6b18a4dc6c595ad2a37d5104753d
Fabian Bützow <fabian@artcom.de>
2009-10-22 15:55:34 +0200

    deleted asset stuff from urihelper

--- Y60Lib/src/com/artcom/y60/UriHelper.java
+++ Y60Lib/src/com/artcom/y60/UriHelper.java
@@ -3,7 +3,6 @@ package com.artcom.y60;
 import android.net.Uri;
 
 public class UriHelper {
-    private static final String LOG_TAG = "UriHelper";
 
     public static String join(String pBaseUri, String... pSegments) {
 
@@ -32,19 +31,4 @@ public class UriHelper {
         return pStr;
     }
 
-    public static boolean isImageUri(String pUri) {
-        return pUri.substring(pUri.lastIndexOf(".") + 1).equals("png")
-                || pUri.substring(pUri.lastIndexOf(".") + 1).equals("jpg")
-                || pUri.substring(pUri.lastIndexOf(".") + 1).equals("jpeg")
-                || pUri.substring(pUri.lastIndexOf(".") + 1).equals("gif");
-    }
-
-    public static boolean isRawUri(String pUri) {
-
-        // String[] rawTypes = { ".png", ".jpg", ".jpeg", ".gif", ".mp4",
-        // ".mp3", ".3gp", ".mov", ".wav" };
-        // String s = pUri.substring(pUri.toLowerCase().lastIndexOf("."));
-        int lastDotIndex = pUri.length() - pUri.lastIndexOf(".") - 1;
-        return lastDotIndex == 3 || lastDotIndex == 4;
-    }
 }
