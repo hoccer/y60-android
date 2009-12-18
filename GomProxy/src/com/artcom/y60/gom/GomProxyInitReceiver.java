0bc088e10f76a490ddca984c850f12dc9623fe14
Fabian Bützow (mobile) <fabian@artcom.de>
2009-12-18 15:25:38 +0100

    removed old runlevel stuff

--- GomProxy/src/com/artcom/y60/gom/GomProxyInitReceiver.java
+++ GomProxy/src/com/artcom/y60/gom/GomProxyInitReceiver.java
@@ -1,9 +1,5 @@
 package com.artcom.y60.gom;
 
-import com.artcom.y60.IntentExtraKeys;
-import com.artcom.y60.Logger;
-import com.artcom.y60.Y60Action;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,14 +11,14 @@ public class GomProxyInitReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context pCtx, Intent pIntent) {
 
-        Intent startGomProxyIntent = new Intent(Y60Action.SERVICE_GOM_PROXY);
-        if (pIntent.hasExtra(IntentExtraKeys.IS_IN_INIT_CHAIN)) {
-            Logger.v(LOG_TAG, "starting gom proxy service, adding extra: ", pIntent
-                    .getBooleanExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, false));
-            startGomProxyIntent.putExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, pIntent.getBooleanExtra(
-                    IntentExtraKeys.IS_IN_INIT_CHAIN, false));
-        }
-        Logger.v(LOG_TAG, "starting gom proxy service");
-        pCtx.startService(startGomProxyIntent);
+        // Intent startGomProxyIntent = new Intent(Y60Action.SERVICE_GOM_PROXY);
+        // if (pIntent.hasExtra(IntentExtraKeys.IS_IN_INIT_CHAIN)) {
+        // Logger.v(LOG_TAG, "starting gom proxy service, adding extra: ", pIntent
+        // .getBooleanExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, false));
+        // startGomProxyIntent.putExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, pIntent.getBooleanExtra(
+        // IntentExtraKeys.IS_IN_INIT_CHAIN, false));
+        // }
+        // Logger.v(LOG_TAG, "starting gom proxy service");
+        // pCtx.startService(startGomProxyIntent);
     }
 }
--- HttpProxy/src/com/artcom/y60/http/HttpProxyInitReceiver.java
+++ HttpProxy/src/com/artcom/y60/http/HttpProxyInitReceiver.java
@@ -1,9 +1,5 @@
 package com.artcom.y60.http;
 
-import com.artcom.y60.IntentExtraKeys;
-import com.artcom.y60.Logger;
-import com.artcom.y60.Y60Action;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,12 +11,12 @@ public class HttpProxyInitReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context pCtx, Intent pIntent) {
 
-        Logger.v(LOG_TAG, "starting http proxy service");
-
-        Intent startHttpProxyIntent = new Intent(Y60Action.SERVICE_HTTP_PROXY);
-        if (pIntent.hasExtra(IntentExtraKeys.IS_IN_INIT_CHAIN)) {
-            startHttpProxyIntent.putExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, true);
-        }
-        pCtx.startService(startHttpProxyIntent);
+        // Logger.v(LOG_TAG, "starting http proxy service");
+        //
+        // Intent startHttpProxyIntent = new Intent(Y60Action.SERVICE_HTTP_PROXY);
+        // if (pIntent.hasExtra(IntentExtraKeys.IS_IN_INIT_CHAIN)) {
+        // startHttpProxyIntent.putExtra(IntentExtraKeys.IS_IN_INIT_CHAIN, true);
+        // }
+        // pCtx.startService(startHttpProxyIntent);
     }
 }
