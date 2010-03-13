3db5a49054d23755c82201bbde6e25c50ea5619c
Rodja Trappe <rodja@artcom.de>
2010-03-13 10:10:40 +0100

    added new exception to signal errors when building an intent

---
+++ Y60Lib/src/com/artcom/y60/error/IntentNotAvailableException.java
@@ -0,0 +1,24 @@
+package com.artcom.y60.error;
+
+public class IntentNotAvailableException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7587278251631568636L;
+    
+    public IntentNotAvailableException(String pMessage) {
+        super(pMessage);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public IntentNotAvailableException(String pDetailMessage, Throwable pThrowable) {
+        super(pDetailMessage, pThrowable);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public IntentNotAvailableException(Throwable pThrowable) {
+        super(pThrowable);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
