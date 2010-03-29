63395360361f16f5ee283266b2d732a316371a38
Rodja Trappe <rodja@artcom.de>
2010-03-29 17:02:21 +0200

    minor tweakings

---
+++ scripts/lib/http_utils.rb
@@ -0,0 +1,33 @@
+require 'hpricot'
+require 'net/https'
+require 'uri'
+
+module Y60Utils
+  
+  def self.fetch_wiki_page uri
+    response = get_page uri
+    html = response.body
+
+    doc = Hpricot html
+    (doc/"div.wikipage").to_s
+  end
+
+  def self.http_get uri
+    #puts "get trac page: #{uri}"
+
+    # https certificate handling code taken from:
+    #   http://notetoself.vrensk.com/2008/09/verified-https-in-ruby/
+    #
+    uri = (URI.parse uri)
+    #puts "uri: #{uri.inspect}"
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == "https"  # enable SSL/TLS
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    #puts "http: #{http}"
+    #puts "uri.path: #{uri.path}"
+    http.start { http.request_get(uri.request_uri) }
+  end
+
+end
