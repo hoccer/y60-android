d304f4e2ef510aabe78d5b7e7aec2cb2c0778ddb
Rodja Trappe <rodja@artcom.de>
2010-03-29 17:27:01 +0200

    remove https

--- scripts/lib/http_utils.rb
+++ scripts/lib/http_utils.rb
@@ -1,4 +1,3 @@
-require 'net/https'
 require 'uri'
 
 module Y60Utils
@@ -12,20 +11,8 @@ module Y60Utils
   end
 
   def self.http_get uri
-    #puts "get trac page: #{uri}"
-
-    # https certificate handling code taken from:
-    #   http://notetoself.vrensk.com/2008/09/verified-https-in-ruby/
-    #
     uri = (URI.parse uri)
-    #puts "uri: #{uri.inspect}"
     http = Net::HTTP.new(uri.host, uri.port)
-    if uri.scheme == "https"  # enable SSL/TLS
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
-    #puts "http: #{http}"
-    #puts "uri.path: #{uri.path}"
     http.start { http.request_get(uri.request_uri) }
   end
 
