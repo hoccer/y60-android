e034b106e2ff7ae910892832a9c100eae1414e10
Rodja Trappe <rodja@artcom.de>
2010-04-01 16:43:31 +0200

    progress must be between 0% and 100%

--- Y60App/tests/com/artcom/y60/thread/TestThreadedTask.java
+++ Y60App/tests/com/artcom/y60/thread/TestThreadedTask.java
@@ -21,8 +21,8 @@ public class TestThreadedTask extends AndroidTestCase {
         assertEquals("task should be in Thread.State.NEW", Thread.State.NEW, mTask.getState());
     }
     
-    public void testProgress() throws Exception {
-        mTask = new ThreadedTask() {
+    public void testProgressWithSaneValues() throws Exception {
+        mTask = new ThreadedTaskForTesting() {
             @Override
             public void run() {
                 setProgress(1);
@@ -33,55 +33,53 @@ public class TestThreadedTask extends AndroidTestCase {
                 sleep();
                 setProgress(100);
             }
-            
-            private void sleep() {
-                try {
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    // this is fine for testing purposes
-                    throw new RuntimeException(e);
-                }
-            }
         };
         
-        assertEquals("task should start with zero progress", 0, mTask.getProgress());
+        assertEquals("task should start with 0% progress", 0, mTask.getProgress());
         mTask.start();
         
-        TestHelper.blockUntilEquals("progress should increase", 300, 1,
-                new TestHelper.Measurement() {
-                    
-                    @Override
-                    public Object getActualValue() throws Exception {
-                        return mTask.getProgress();
-                    }
-                });
-        
-        TestHelper.blockUntilEquals("progress should increase", 300, 53,
-                new TestHelper.Measurement() {
-                    
-                    @Override
-                    public Object getActualValue() throws Exception {
-                        return mTask.getProgress();
-                    }
-                });
-        
-        TestHelper.blockUntilEquals("progress should increase", 300, 99,
-                new TestHelper.Measurement() {
-                    
-                    @Override
-                    public Object getActualValue() throws Exception {
-                        return mTask.getProgress();
-                    }
-                });
-        
-        TestHelper.blockUntilEquals("progress should increase", 300, 100,
-                new TestHelper.Measurement() {
-                    
-                    @Override
-                    public Object getActualValue() throws Exception {
-                        return mTask.getProgress();
-                    }
-                });
-        
+        assertProgress("progress should increase", 1);
+        assertProgress("progress should increase", 53);
+        assertProgress("progress should increase", 99);
+        assertProgress("progress should increase", 100);
     }
+    
+    public void testProgressWithBadValues() throws Exception {
+        mTask = new ThreadedTaskForTesting() {
+            @Override
+            public void run() {
+                setProgress(-1);
+                sleep();
+                setProgress(2011);
+            }
+        };
+        assertEquals("task should start with 0% progress", 0, mTask.getProgress());
+        mTask.start();
+        Thread.sleep(100);
+        assertEquals("task should stay at 0% progress", 0, mTask.getProgress());
+        Thread.sleep(100);
+        assertEquals("task should clamp at 100% progress", 100, mTask.getProgress());
+    }
+    
+    private void assertProgress(String pMessage, int pExpectedPercent) throws Exception {
+        TestHelper.blockUntilEquals(pMessage, 300, pExpectedPercent, new TestHelper.Measurement() {
+            @Override
+            public Object getActualValue() throws Exception {
+                return mTask.getProgress();
+            }
+        });
+    }
+    
+    private abstract class ThreadedTaskForTesting extends ThreadedTask {
+        
+        protected void sleep() {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                // this is fine for testing purposes
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
 }
