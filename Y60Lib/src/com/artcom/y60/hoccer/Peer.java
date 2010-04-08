94625109049f93827631552292cf146e9b33e4d5
Rodja Trappe <rodja@artcom.de>
2010-04-08 07:32:48 +0200

    started with upload/PUT unit tests

--- Y60Lib/src/com/artcom/y60/hoccer/Peer.java
+++ Y60Lib/src/com/artcom/y60/hoccer/Peer.java
@@ -1,17 +1,13 @@
 package com.artcom.y60.hoccer;
 
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 
 public class Peer {
     
     DefaultHttpClient mHttpClient;
     
     public Peer(String clientName) {
-        HttpParams httpParams = new BasicHttpParams();
-        // HttpClientParams.setRedirecting(httpParams, false);
-        mHttpClient = new DefaultHttpClient(httpParams);
+        mHttpClient = new DefaultHttpClient();
         mHttpClient.getParams().setParameter("http.useragent", clientName);
     }
     
