c56059efbaa73bef5b1a1b5d424272857f0b526f
Rodja Trappe <rodja@artcom.de>
2010-04-09 15:24:49 +0200

    added basic http parms to peers httpclient -- won't work otherwise

--- Y60Lib/src/com/artcom/y60/hoccer/Peer.java
+++ Y60Lib/src/com/artcom/y60/hoccer/Peer.java
@@ -1,13 +1,14 @@
 package com.artcom.y60.hoccer;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
 
 public class Peer {
     
     DefaultHttpClient mHttpClient;
     
     public Peer(String clientName) {
-        mHttpClient = new DefaultHttpClient();
+        mHttpClient = new DefaultHttpClient(new BasicHttpParams());
         mHttpClient.getParams().setParameter("http.useragent", clientName);
     }
     
