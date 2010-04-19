33d56b4a93b58025e7228c9623171264113d2b32
Robert Palmer <robert.palmer@artcom.de>
2010-04-19 12:29:32 +0200

    calculating the hoccability

--- Y60Lib/src/com/artcom/y60/hoccer/HocLocation.java
+++ Y60Lib/src/com/artcom/y60/hoccer/HocLocation.java
@@ -9,20 +9,20 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class HocLocation extends Location {
-    
+
     private List<ScanResult> mScanResults = null;
-    
+
     private HocLocation(Location pLocation) {
         super(pLocation);
     }
-    
+
     private HocLocation(Parcel pIn) {
         super(Location.CREATOR.createFromParcel(pIn));
         mScanResults = new ArrayList<ScanResult>();
-        
+
         pIn.readList(mScanResults, null);
     }
-    
+
     public static HocLocation createFromLocation(Location pLocation, List<ScanResult> pScanResults) {
         if (pLocation == null) {
             return null;
@@ -32,32 +32,48 @@ public class HocLocation extends Location {
             return hocLocation;
         }
     }
-    
+
     public List<ScanResult> getScanResults() {
         return mScanResults;
     }
-    
+
     public void setScanResult(List<ScanResult> pScanResults) {
         mScanResults = pScanResults;
     }
-    
+
+    public int getHoccability() {
+        int hoccability = 0;
+
+        if (mScanResults != null && mScanResults.size() > 0) {
+            hoccability += 1;
+        }
+
+        if (getAccuracy() < 200) {
+            hoccability += 2;
+        } else if (getAccuracy() < 2000) {
+            hoccability += 1;
+        }
+
+        return hoccability;
+    }
+
     @Override
     public void writeToParcel(Parcel pDest, int pFlags) {
         super.writeToParcel(pDest, pFlags);
-        
+
         pDest.writeList(mScanResults);
     }
-    
+
     public static final Parcelable.Creator<HocLocation> CREATOR = new Parcelable.Creator<HocLocation>() {
                                                                     public HocLocation createFromParcel(
                                                                             Parcel in) {
                                                                         return new HocLocation(in);
                                                                     }
-                                                                    
+
                                                                     public HocLocation[] newArray(
                                                                             int size) {
                                                                         return new HocLocation[size];
                                                                     }
                                                                 };
-    
+
 }
