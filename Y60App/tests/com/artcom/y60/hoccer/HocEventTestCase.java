9a3fa2bcdc9795eee489ef4872e4f9ba9176d939
Fabian Bützow (mobile) <fabian@artcom.de>
2010-04-20 12:31:18 +0200

    tests for sending arbitrary StreamableContent

--- Y60App/tests/com/artcom/y60/hoccer/HocEventTestCase.java
+++ Y60App/tests/com/artcom/y60/hoccer/HocEventTestCase.java
@@ -10,11 +10,11 @@ import com.artcom.y60.Logger;
 import com.artcom.y60.TestHelper;
 
 public class HocEventTestCase extends TestCase {
-    
+
     private static String      LOG_TAG   = "HocEventTestCase";
     private static HocLocation mLocation = null;
     private Peer               mPeer;
-    
+
     public HocEventTestCase() {
         if (mLocation == null) {
             Logger
@@ -22,37 +22,37 @@ public class HocEventTestCase extends TestCase {
                             "created a default HocLocation -- all tests need to use getUniqueHocLocation()");
             Location location = new Location("TestLocationProvider");
             location.setAccuracy(100);
-            location.setLatitude(20);
-            location.setLongitude(20);
+            location.setLatitude((Math.random() + 0.1) * 20);
+            location.setLongitude((Math.random() + 0.1) * 20);
             mLocation = HocLocation.createFromLocation(location, new ArrayList<ScanResult>());
         }
     }
-    
+
     @Override
     public void setUp() {
         mPeer = new Peer("Y60/Hoccer Unit Test on Android");
         mPeer.setLocation(getUniqueHocLocation());
     }
-    
+
     @Override
     public void tearDown() {
         mPeer = null;
     }
-    
+
     protected Peer getPeer() {
         return mPeer;
     }
-    
+
     protected HocLocation getUniqueHocLocation() {
         mLocation.setLatitude(mLocation.getLatitude() + 0.1);
         mLocation.setLongitude(mLocation.getLongitude() + 0.1);
         return mLocation;
     }
-    
+
     protected void assertEventIsAlive(String pEventName, final HocEvent pEvent) throws Exception {
         TestHelper.blockUntilTrue(pEventName + " event should have been created", 10000,
                 new TestHelper.Condition() {
-                    
+
                     @Override
                     public boolean isSatisfied() throws Exception {
                         assertFalse("event should not collide with others", pEvent.hasCollision());
@@ -60,28 +60,28 @@ public class HocEventTestCase extends TestCase {
                     }
                 });
     }
-    
+
     protected void assertEventIsExpired(String pEventName, final HocEvent pEvent) throws Exception {
         TestHelper.blockUntilFalse(pEventName + " event shuld be expired by now", 10000,
                 new TestHelper.Condition() {
-                    
+
                     @Override
                     public boolean isSatisfied() throws Exception {
                         return pEvent.isAlive();
                     }
                 });
     }
-    
+
     protected void assertEventHasNumberOfPeers(final HocEvent pEvent, int expectedPeerCount)
             throws Exception {
         TestHelper.blockUntilEquals(" HocEvent shuld have " + expectedPeerCount + " peers by now",
                 7000, expectedPeerCount, new TestHelper.Measurement() {
-                    
+
                     @Override
                     public Object getActualValue() throws Exception {
                         return pEvent.getLinkedPeerCount();
                     }
                 });
     }
-    
+
 }
