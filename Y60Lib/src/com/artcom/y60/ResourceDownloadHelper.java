7c6a895491242ed634199404ae9d637d025777cd
Fabian Bützow (mobile) <fabian@artcom.de>
2010-04-26 16:37:29 +0200

    logs for writing IO on sdcard

--- Y60Lib/src/com/artcom/y60/ResourceDownloadHelper.java
+++ Y60Lib/src/com/artcom/y60/ResourceDownloadHelper.java
@@ -3,34 +3,35 @@ package com.artcom.y60;
 import java.io.IOException;
 import java.util.UUID;
 
-import android.net.Uri;
-import android.os.Bundle;
-
 import com.artcom.y60.http.HttpClientException;
 import com.artcom.y60.http.HttpHelper;
 import com.artcom.y60.http.HttpProxyConstants;
 import com.artcom.y60.http.HttpServerException;
 
+import android.net.Uri;
+import android.os.Bundle;
+
 public class ResourceDownloadHelper {
-    
+
     static final String LOG_TAG = ResourceDownloadHelper.class.getName();
-    
+
     public static Bundle downloadAndCreateResourceBundle(String pBasePath, String pUri)
             throws IOException, HttpClientException, HttpServerException {
         long size = HttpHelper.getSize(pUri);
         Bundle newContent = new Bundle(2);
         newContent.putLong(HttpProxyConstants.SIZE_TAG, size);
-        
+
         if (size > HttpProxyConstants.MAX_IN_MEMORY_SIZE) {
             String localResourcePath = pBasePath + UUID.randomUUID();
-            
+            Logger.v(LOG_TAG, "before writing to sdcard: ", localResourcePath);
             HttpHelper.fetchUriToFile(pUri, localResourcePath);
             newContent.putString(HttpProxyConstants.LOCAL_RESOURCE_PATH_TAG, localResourcePath);
+            Logger.v(LOG_TAG, "after writing to sdcard: ", localResourcePath);
         } else {
             byte[] array = HttpHelper.getAsByteArray(Uri.parse(pUri));
             newContent.putByteArray(HttpProxyConstants.BYTE_ARRAY_TAG, array);
         }
         return newContent;
     }
-    
+
 }
