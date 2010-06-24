a7e12cc8b9ddf30c704a5a90f378d48e1b130049
Fabian Bützow (mobile) <fabian@artcom.de>
2010-06-24 14:18:37 +0200

    removed some logs

--- Y60Lib/src/com/artcom/y60/ResourceDownloadHelper.java
+++ Y60Lib/src/com/artcom/y60/ResourceDownloadHelper.java
@@ -16,7 +16,6 @@ public class ResourceDownloadHelper {
 
     public static Bundle downloadAndCreateResourceBundle(String pBasePath, String pUri)
             throws IOException, HttpClientException, HttpServerException {
-        // long size = HttpHelper.getSize(pUri);
         Logger.v(LOG_TAG, "downloading content");
         byte[] arrayTmp = HttpHelper.getAsByteArray(pUri);
         Logger.v(LOG_TAG, "downloaded content");
@@ -28,14 +27,10 @@ public class ResourceDownloadHelper {
         if (size > HttpProxyConstants.MAX_IN_MEMORY_SIZE) {
             String localResourcePath = pBasePath + UUID.randomUUID();
             Logger.v(LOG_TAG, "before writing to sdcard: ", localResourcePath);
-            // HttpHelper.fetchUriToFile(pUri, localResourcePath);
             IoHelper.writeByteArrayToFile(arrayTmp, localResourcePath);
             newContent.putString(HttpProxyConstants.LOCAL_RESOURCE_PATH_TAG, localResourcePath);
             Logger.v(LOG_TAG, "after writing to sdcard: ", localResourcePath);
         } else {
-            // Logger.v(LOG_TAG, "downloading small content");
-            // byte[] array = HttpHelper.getAsByteArray(pUri);
-            // Logger.v(LOG_TAG, "downloaded small content");
             newContent.putByteArray(HttpProxyConstants.BYTE_ARRAY_TAG, arrayTmp);
         }
         return newContent;
