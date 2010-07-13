56ff575e86353a957413a8ac32532ea523c4a3a7
Robert Palmer <robert.palmer@artcom.de>
2010-07-13 13:08:36 +0200

    better error message for no internet connection error

--- Y60Lib/src/com/artcom/y60/hoccer/HocEventException.java
+++ Y60Lib/src/com/artcom/y60/hoccer/HocEventException.java
@@ -1,8 +1,7 @@
 package com.artcom.y60.hoccer;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.net.UnknownHostException;
 
 import org.apache.http.HttpResponse;
 import org.json.JSONException;
@@ -12,37 +11,38 @@ import com.artcom.y60.Logger;
 import com.artcom.y60.http.HttpHelper;
 
 public class HocEventException extends Exception {
-    
+
     private static final long   serialVersionUID = 1L;
-    
+
     private static final String LOG_TAG          = "HocEventException";
-    
+
     private String              mState;
     private String              mMessage;
     private final String        mUrl;
-    
+
     public HocEventException(Exception e) {
         super(e);
         mUrl = "";
-        mMessage = e.getMessage();
+
+        if (e instanceof UnknownHostException) {
+            mMessage = "Could not connect to server";
+        } else {
+            mMessage = e.getMessage();
+        }
         // not correctly implemented right now
     }
-    
+
     public HocEventException(String pUrl, HttpResponse pResponse) {
         mUrl = pUrl;
-        
+
         JSONObject json;
         try {
-            
+
             String content = HttpHelper.extractBodyAsString(pResponse.getEntity());
-            
-            File file = new File("/sdcard/HoccerServerException.xml");
-            RandomAccessFile raf = new RandomAccessFile(file, "rw");
-            raf.write(content.getBytes());
-            
+
             json = new JSONObject(content);
             Logger.v(LOG_TAG, json);
-            
+
             mState = json.getString("state");
             if (json.has("message")) {
                 mMessage = json.getString("message");
@@ -52,32 +52,32 @@ public class HocEventException extends Exception {
         } catch (IOException e) {
             Logger.v(LOG_TAG, e);
         }
-        
+
     }
-    
+
     public HocEventException(String message, String state, String url) {
         mState = state;
         mMessage = message;
         mUrl = url;
     }
-    
+
     public String getState() {
         return mState;
     }
-    
+
     @Override
     public String getMessage() {
         return mMessage;
     }
-    
+
     public String getBestReadableMessage() {
         if (getMessage() != null) {
             return getMessage();
         }
-        
+
         return getState();
     }
-    
+
     public String getUrl() {
         return mUrl;
     }
