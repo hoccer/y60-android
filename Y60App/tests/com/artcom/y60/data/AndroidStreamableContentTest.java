8420c18b8a2dd859045aa9d93c4a35f6ae7bdab6
Fabian Bützow (mobile) <fabian@artcom.de>
2010-07-22 09:57:54 +0200

    test for file uri scheme streamables

--- Y60App/tests/com/artcom/y60/data/AndroidStreamableContentTest.java
+++ Y60App/tests/com/artcom/y60/data/AndroidStreamableContentTest.java
@@ -2,6 +2,9 @@ package com.artcom.y60.data;
 
 import android.test.AndroidTestCase;
 
+import com.artcom.y60.FileTestHelper;
+import com.artcom.y60.FileTestHelper.TestFile;
+
 public class AndroidStreamableContentTest extends AndroidTestCase {
 
     private static final String LOG_TAG = "AndroidStreamableContentTest";
@@ -14,8 +17,16 @@ public class AndroidStreamableContentTest extends AndroidTestCase {
     }
 
     public void testSettingFileScheme() throws Exception {
+        TestFile testfile = new FileTestHelper().new TextFile("mannoman".getBytes(), "text/plain",
+                "txt");
+
         GenericAndroidStreamableContent streamableContent = new GenericAndroidStreamableContent(
                 getContext().getContentResolver());
+        streamableContent.setDataUri(testfile.getFileSchemeUri());
+
+        assertEquals(testfile.getFileSchemeUri(), streamableContent.getDataUri());
+        assertEquals(testfile.mContent.length, streamableContent.getStreamLength());
+        assertEquals(testfile.mContentType, streamableContent.getContentType());
 
     }
 }
