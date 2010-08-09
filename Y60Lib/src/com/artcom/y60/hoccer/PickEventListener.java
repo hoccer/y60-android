36f07333667971de087de83b881d43136c303ddf
Robert Palmer <robert.palmer@artcom.de>
2010-08-09 14:25:52 +0200

    added PickEventListener

---
+++ Y60Lib/src/com/artcom/y60/hoccer/PickEventListener.java
@@ -0,0 +1,10 @@
+package com.artcom.y60.hoccer;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PickEventListener {
+
+    public void onPickableDownloadListChanged(List<Map<String, String>> pieces);
+
+}
