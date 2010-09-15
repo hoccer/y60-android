2f4a1e0135c11156862c631d7cc58fc287590fcb
Christian Bunk <christian.bunk@student.htw-berlin.de>
2010-09-15 16:48:54 +0200

    add message for UnknownLocationException

--- Y60Lib/src/com/artcom/y60/hoccer/UnknownLocationException.java
+++ Y60Lib/src/com/artcom/y60/hoccer/UnknownLocationException.java
@@ -1,7 +1,11 @@
 package com.artcom.y60.hoccer;
 
 public class UnknownLocationException extends Exception {
-    
+
     private static final long serialVersionUID = -8695072751079768808L;
-    
+
+    public UnknownLocationException() {
+        super("Location is unknown");
+    }
+
 }
