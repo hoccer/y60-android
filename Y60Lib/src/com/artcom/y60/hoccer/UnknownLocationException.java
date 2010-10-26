891c07cbfec7372c32438f3159c44587572c0851
Fabian Bützow (mobile) <fabian@artcom.de>
2010-10-26 13:07:12 +0200

    added constructors for unknown location exception

--- Y60Lib/src/com/artcom/y60/hoccer/UnknownLocationException.java
+++ Y60Lib/src/com/artcom/y60/hoccer/UnknownLocationException.java
@@ -1,7 +1,27 @@
 package com.artcom.y60.hoccer;
 
 public class UnknownLocationException extends Exception {
-    
+
+    public UnknownLocationException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public UnknownLocationException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+        // TODO Auto-generated constructor stub
+    }
+
+    public UnknownLocationException(String detailMessage) {
+        super(detailMessage);
+        // TODO Auto-generated constructor stub
+    }
+
+    public UnknownLocationException(Throwable throwable) {
+        super(throwable);
+        // TODO Auto-generated constructor stub
+    }
+
     private static final long serialVersionUID = -8695072751079768808L;
-    
+
 }
