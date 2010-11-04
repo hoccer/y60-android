c7e95dd595f76d3702cafe8ed190a4e265dae0e2
Andreas Marr <andreasm@artcom.de>
2010-11-04 16:27:25 +0100

    minor cleanup

--- scripts/copy_theme.rb
+++ scripts/copy_theme.rb
@@ -8,13 +8,11 @@ require 'java_project'
 require 'android_project'
 
 def main action, pj_names
-  
   projects = Project.load_in_dependency_order(pj_names)
   projects = projects.select { |p| p.is_t_gallery_project? and File.exist? p.path+"/res" } 
   
   sourceProject = Project.find_or_create "TgCommonLib"
 
-  
   projects.each do |project| 
      Dir[sourceProject.path+"/theme/*/*"].each do |file| 
        fileParts = file.split("/")
@@ -30,9 +28,6 @@ def main action, pj_names
        
      end
   end
-
-  
-  
 rescue => e
   puts "oops: #{e}\n#{e.backtrace.join "\n"}"
   exit 1
