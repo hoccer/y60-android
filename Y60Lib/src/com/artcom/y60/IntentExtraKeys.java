b42106f7909236fc8555014b01fb8b4cacef1d4a
Fabian Bützow (mobile) <fabian@artcom.de>
2010-12-02 13:28:13 +0100

    added boot up service intent extra keys

--- Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
+++ Y60Lib/src/com/artcom/y60/IntentExtraKeys.java
@@ -1,60 +1,64 @@
 package com.artcom.y60;
 
 public class IntentExtraKeys {
-    public static final String CALLING_APP              = "calling_app";
-    public static final String QUERY_PATH               = "query_path";
-    public static final String QUERY_TITLE              = "query_title";
-    public static final String RCI_TARGET               = "target";
-    public static final String RCI_SENDER               = "sender";
-    public static final String RCI_RECEIVER             = "receiver";
-    public static final String RCI_ARGUMENTS            = "rci_arguments";
-    public static final String RCI_FILE_LOCATION        = "rci_file_location";
+    public static final String CALLING_APP               = "calling_app";
+    public static final String QUERY_PATH                = "query_path";
+    public static final String QUERY_TITLE               = "query_title";
+    public static final String RCI_TARGET                = "target";
+    public static final String RCI_SENDER                = "sender";
+    public static final String RCI_RECEIVER              = "receiver";
+    public static final String RCI_ARGUMENTS             = "rci_arguments";
+    public static final String RCI_FILE_LOCATION         = "rci_file_location";
 
-    public static final String NOTIFICATION_PATH        = "uri";
-    public static final String NOTIFICATION_OPERATION   = "operation";
-    public static final String NOTIFICATION_DATA_STRING = "data";
-    public static final String WEB_URL                  = "web_url";
+    public static final String NOTIFICATION_PATH         = "uri";
+    public static final String NOTIFICATION_OPERATION    = "operation";
+    public static final String NOTIFICATION_DATA_STRING  = "data";
+    public static final String WEB_URL                   = "web_url";
 
-    public static final String QUESTION_HEADER          = "header";
-    public static final String QUESTION_QUESTION        = "question";
-    public static final String QUESTION_OPTION_A        = "optiona";
-    public static final String QUESTION_OPTION_B        = "optionb";
-    public static final String QUESTION_ACTION_A        = "actiona";
-    public static final String QUESTION_ACTION_B        = "actionb";
-    public static final String QUESTION_IMAGE           = "image";
-    public static final String QUESTION_UUID            = "uuid";
+    public static final String QUESTION_HEADER           = "header";
+    public static final String QUESTION_QUESTION         = "question";
+    public static final String QUESTION_OPTION_A         = "optiona";
+    public static final String QUESTION_OPTION_B         = "optionb";
+    public static final String QUESTION_ACTION_A         = "actiona";
+    public static final String QUESTION_ACTION_B         = "actionb";
+    public static final String QUESTION_IMAGE            = "image";
+    public static final String QUESTION_UUID             = "uuid";
 
-    public static final String ASSET                    = "asset";
-    public static final String HAS_FOLLOW_UP_ACTION     = "has_follow_up_action";
+    public static final String ASSET                     = "asset";
+    public static final String HAS_FOLLOW_UP_ACTION      = "has_follow_up_action";
 
-    public static final String VOICE_CONTROL_REASON     = "reason";
-    public static final String RETURN_DATA              = "return_data";
-    public static final String RETURN_DATA_LIST         = "return_data_list";
-    public static final String RETURN_SUCCESS           = "return_success";
-    public static final String RETURN_SERVICE           = "return_service";
-    public static final String REPORT_SINGLE            = "report_single";
-    public static final String ACTION                   = "action";
-    public static final String RECIPIENT                = "recipient";
+    public static final String VOICE_CONTROL_REASON      = "reason";
+    public static final String RETURN_DATA               = "return_data";
+    public static final String RETURN_DATA_LIST          = "return_data_list";
+    public static final String RETURN_SUCCESS            = "return_success";
+    public static final String RETURN_SERVICE            = "return_service";
+    public static final String REPORT_SINGLE             = "report_single";
+    public static final String ACTION                    = "action";
+    public static final String RECIPIENT                 = "recipient";
 
-    public static final String BROADCAST_SENDER_CLASS   = "broadcast_sender_class";
+    public static final String BROADCAST_SENDER_CLASS    = "broadcast_sender_class";
 
-    public static final String DETAIL                   = "detail";
-    public static final String MODE                     = "mode";
-    public static final String VICO_STREAM_URI_GOM_PATH = "vico_stream_uri_gom_path";
-    public static final String IS_IN_INIT_CHAIN         = "is_in_init_chain";
-    public static final String TRACK_NUMBER             = "track_number";
-    public static final String TRACK_POSITION           = "track_position";
+    public static final String DETAIL                    = "detail";
+    public static final String MODE                      = "mode";
+    public static final String VICO_STREAM_URI_GOM_PATH  = "vico_stream_uri_gom_path";
+    public static final String IS_IN_INIT_CHAIN          = "is_in_init_chain";
+    public static final String TRACK_NUMBER              = "track_number";
+    public static final String TRACK_POSITION            = "track_position";
 
-    public static final String PROFILING                = "profiling";
+    public static final String PROFILING                 = "profiling";
 
-    public static final String INIT_PROGRESS            = "progress";
-    public static final String INIT_LOG_MSG             = "init_log_msg";
-    public static final String INIT_IN_DEBUG_MODE       = "init_in_debug_mode";
-    public static final String INIT_CHECK_SERVICE       = "init_check_service";
-    public static final String SEARCH_QUERY             = "search_query";
-    public static final String ODP_AGC_ENABLED          = "odp_agc_enabled";
-    public static final String EXCLUDE_LIST             = "exclude_list";
-    public static final String POSSESSION_PATH          = "possession_path";
-    public static final String WIDGET_ARGS_PATH         = "widget_args_path";
-    
+    public static final String INIT_PROGRESS             = "progress";
+    public static final String INIT_LOG_MSG              = "init_log_msg";
+    public static final String INIT_IN_DEBUG_MODE        = "init_in_debug_mode";
+    public static final String INIT_CHECK_SERVICE        = "init_check_service";
+    public static final String SEARCH_QUERY              = "search_query";
+    public static final String ODP_AGC_ENABLED           = "odp_agc_enabled";
+    public static final String EXCLUDE_LIST              = "exclude_list";
+    public static final String POSSESSION_PATH           = "possession_path";
+    public static final String WIDGET_ARGS_PATH          = "widget_args_path";
+
+    public static final String BOOT_UP_MEDIA_MOUNTED     = "boot_up_media_mounted";
+    public static final String BOOT_UP_NETWORK_CONNECTED = "boot_up_network_connected";
+    public static final String BOOT_UP_DEVICE_BOOTED     = "boot_up_device_booted";
+
 }
