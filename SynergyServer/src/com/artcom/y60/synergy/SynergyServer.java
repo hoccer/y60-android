bb11e1bf5721c74e6f5d5753ed7be049b7e87a4e
Sinan Goo <sinan.goo@artcom.de>
2010-12-22 17:33:19 +0100

    configure SynergyServer as simple Html Server

--- SynergyServer/src/com/artcom/y60/synergy/SynergyServer.java
+++ SynergyServer/src/com/artcom/y60/synergy/SynergyServer.java
@@ -1,13 +1,139 @@
 package com.artcom.y60.synergy;
 
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.handler.HandlerCollection;
+import org.mortbay.thread.QueuedThreadPool;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.artcom.y60.ErrorHandling;
+import com.artcom.y60.Logger;
+
+
 public class SynergyServer extends Activity {
-    /** Called when the activity is first created. */
+
+    private static final String     LOG_TAG          = "SynergyServer";
+
+    private Server                  mServer;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logger.v(LOG_TAG, ">>> onCreate() ", this);
         setContentView(R.layout.main);
+        try {
+            if (mServer == null) {
+                mServer = startServer(4043);
+            }
+        } catch (Exception ex) {
+            ErrorHandling.signalUnspecifiedError(LOG_TAG, ex, this);
+        }
+        Logger.v(LOG_TAG, "<<< onCreate() ", this);
     }
+
+    @Override
+    protected void onStart() {
+        Logger.v(LOG_TAG, ">>> onStart() ", this);
+        super.onStart();
+        Logger.v(LOG_TAG, "<<< onStart() ", this);
+    }
+
+    @Override
+    protected void onRestart() {
+        Logger.v(LOG_TAG, ">>> onRestart() ", this);
+        super.onRestart();
+        Logger.v(LOG_TAG, "<<< onRestart() ", this);
+    }
+
+    @Override
+    protected void onResume() {
+        Logger.v(LOG_TAG, ">>> onResume() ", this);
+        super.onResume();
+        Logger.v(LOG_TAG, "<<< onResume() ", this);
+    }
+
+    @Override
+    protected void onPostResume() {
+        Logger.v(LOG_TAG, ">>> onPostResume() ", this);
+        super.onPostResume();
+        Logger.v(LOG_TAG, "<<< onPostResume() ", this);
+    }
+
+    @Override
+    public void onPause() {
+        Logger.v(LOG_TAG, ">>> onPause() ", this);
+        super.onPause();
+        Logger.v(LOG_TAG, "<<< onPause() ", this);
+    }
+
+    @Override
+    protected void onStop() {
+        Logger.v(LOG_TAG, ">>> onStop() ", this);
+        super.onStop();
+        Logger.v(LOG_TAG, "<<< onStop() ", this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Logger.v(LOG_TAG, ">>> onDestroy() ", this);
+        if (mServer != null) {
+            try {
+                Logger.v(LOG_TAG, "stopping Jetty");
+                stopServer();
+            } catch (Exception e) {
+                ErrorHandling.signalServiceError(LOG_TAG, e, this);
+            }
+        }
+        super.onDestroy();
+        Logger.v(LOG_TAG, "<<< onDestroy() ", this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent pIntent) {
+        Logger.v(LOG_TAG, ">>> onNewIntent() ", this);
+        super.onNewIntent(pIntent);
+        Logger.v(LOG_TAG, "<<< onNewIntent() ", this);
+    }
+
+    private void stopServer() throws Exception {
+        Logger.i(LOG_TAG, "stopServer(): Jetty Server stopping");
+        mServer.stop();
+        mServer.join();
+        Logger.i(LOG_TAG, "stopServer(): Jetty Server stopped. done.");
+        mServer = null;
+    }
+
+    Server startServer(int pPort) throws Exception {
+        SocketConnector connector = new SocketConnector();
+        connector.setPort(pPort);
+        connector.setHost("0.0.0.0"); // listen on all interfaces
+
+        Server server = new Server();
+        server.setConnectors(new Connector[] { connector });
+
+        // Bridge Jetty logging to Android logging
+        // System.setProperty("org.mortbay.log.class",
+        // "org.mortbay.log.AndroidLog");
+        // org.mortbay.log.Log.setLog(new AndroidLog());
+
+        HandlerCollection handlers = new HandlerCollection();
+        handlers.setHandlers(new Handler[] { new SynergyServerHandler() });
+        server.setHandler(handlers);
+
+        server.start();
+        QueuedThreadPool threadpool = (QueuedThreadPool) server.getThreadPool();
+        threadpool.setMaxStopTimeMs(10);
+
+        return server;
+    }
+
+
+
+
+
 }
---
+++ SynergyServer/src/com/artcom/y60/synergy/SynergyServerHandler.java
@@ -0,0 +1,90 @@
+package com.artcom.y60.synergy;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.jetty.MimeTypes;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.util.ByteArrayISO8859Writer;
+
+import com.artcom.y60.Logger;
+
+public class SynergyServerHandler extends DefaultHandler {
+    private static final String LOG_TAG          = "SynergyServerHandler";
+
+    public SynergyServerHandler() {
+    }
+
+    @Override
+    public void handle(String pTarget, HttpServletRequest pRequest, 
+            HttpServletResponse pResponse, int pDispatch) {
+        try {
+            String method = pRequest.getMethod();
+            Logger.v(LOG_TAG, "Incoming HTTP request________",method);
+            if ("HEAD".equals(method) || "GET".equals(method)) {
+                responseWithHtml(pResponse);
+            } else {
+                Logger.v(LOG_TAG, "Not supported");
+                respondNotImplemented(pResponse);
+            }
+
+        } catch (Exception ex) {
+            //ErrorHandling.signalUnspecifiedError(LOG_TAG, ex, this);
+
+        } finally {
+            if (pRequest instanceof Request) {
+                ((Request) pRequest).setHandled(true);
+            } else {
+                HttpConnection connection = HttpConnection.getCurrentConnection();
+                // HACK: if this is called in a test, the request will be mocked
+                // and
+                // thus the connection will be null
+                if (connection != null) {
+                    connection.getRequest().setHandled(true);
+                }
+            }
+        }
+    }
+
+    private void responseWithHtml(HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType(MimeTypes.TEXT_HTML);
+        response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+
+        ByteArrayISO8859Writer writer = new ByteArrayISO8859Writer(1500);
+        writer.write("<HTML>\n<HEAD>\n<TITLE>Error 404 - Not Found");
+        writer.write("</TITLE>\n<BODY>\n<H2>Error 404 - Not Found.</H2>\n");
+        writer.write("\n</BODY>\n</HTML>\n");
+        writer.flush();
+        response.setContentLength(writer.size());
+        OutputStream out=response.getOutputStream();
+        writer.writeTo(out);
+        out.close();
+    }
+
+    private void respondNotImplemented(HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("text/plain");
+        response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+        response.setContentLength(0);
+    }
+
+    private void respondNotFound(HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        response.setContentLength(0);
+    }
+
+}
+
+
+
+
+
+
