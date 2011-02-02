46e59d1c96169319524f4fb0337c40fc067856d6
Fabian Bützow (mobile) <fabian@artcom.de>
2011-02-02 17:47:38 +0100

    bc is sent when vnc server is started

--- VncServer/src/com/artcom/y60/vncserver/VncService.java
+++ VncServer/src/com/artcom/y60/vncserver/VncService.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import com.artcom.y60.ErrorHandling;
 import com.artcom.y60.IoHelper;
 import com.artcom.y60.Logger;
+import com.artcom.y60.Y60Action;
 import com.artcom.y60.Y60Service;
 
 import android.app.Notification;
@@ -16,16 +17,17 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class VncService extends Y60Service {
-    
+
     private static final String LOG_TAG         = "VNCService";
-    private static final int    NOTIFICATION_ID = 123; // TODO receive from constants?
+    private static final int    NOTIFICATION_ID = 123;               // TODO receive from
+                                                                      // constants?
     private static final String PORT            = "5900";
     private static final String SCALING         = "100";
     private static final String ROTATION        = "0";
     private static final String VNC_EXECUTABLE  = "androidvncserver";
-    
-    private Notification notification = null;
-    
+
+    private Notification        notification    = null;
+
     @Override
     public void onCreate() {
         Logger.v(LOG_TAG, "onCreate()");
@@ -36,7 +38,7 @@ public class VncService extends Y60Service {
         startForeground(NOTIFICATION_ID, notification);
         super.onCreate();
     }
-    
+
     @Override
     public void onDestroy() {
         Logger.v(LOG_TAG, "onDestroy()");
@@ -49,7 +51,7 @@ public class VncService extends Y60Service {
         notificationManager.cancel(NOTIFICATION_ID);
         super.onDestroy();
     }
-    
+
     @Override
     public IBinder onBind(Intent intent) {
         Logger.v(LOG_TAG, "onBind: " + intent);
@@ -58,20 +60,22 @@ public class VncService extends Y60Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Logger.v(LOG_TAG, "onStartCommand  - id: " + startId); //, id: " + startId + " " + this, " " + Thread.currentThread());
+        Logger.v(LOG_TAG, "onStartCommand  - id: " + startId); // , id: " + startId + " " + this, "
+                                                               // " + Thread.currentThread());
         Logger.v(LOG_TAG, "start intent: " + intent);
         try {
             startServer();
+            sendBroadcast(new Intent(Y60Action.VNC_SERVICE_READY));
         } catch (Exception e) {
             ErrorHandling.signalError(LOG_TAG, e, this, ErrorHandling.Category.COMMAND_EXECUTION);
         }
-        
+
         return START_NOT_STICKY;
     }
-    
+
     private Notification getCurrentNotification() {
         int myIcon = R.drawable.icon_neutral;
-        
+
         String myMsg = "Server is";
         if (isVncServerRunning()) {
             myMsg += " started";
@@ -80,18 +84,19 @@ public class VncService extends Y60Service {
             myMsg += " stopped";
             myIcon = R.drawable.icon_stopped;
         }
-        //Notification notification = new Notification(myIcon, LOG_TAG,
-        //        System.currentTimeMillis());
+        // Notification notification = new Notification(myIcon, LOG_TAG,
+        // System.currentTimeMillis());
         notification.icon = myIcon;
         notification.setLatestEventInfo(this, LOG_TAG, myMsg,
                 PendingIntent.getBroadcast(this, 0, new Intent(this, VncService.class), 0));
         return notification;
     }
-    
+
     private boolean isVncServerRunning() {
-        IoHelper.ProcessStates myStatus =  IoHelper.isProcessRunning(VNC_EXECUTABLE);
+        IoHelper.ProcessStates myStatus = IoHelper.isProcessRunning(VNC_EXECUTABLE);
         if (myStatus.equals(IoHelper.ProcessStates.UNKNOWN)) {
-            ErrorHandling.signalIOError(LOG_TAG, new IOException("Unknown status of processs " + VNC_EXECUTABLE), this);
+            ErrorHandling.signalIOError(LOG_TAG, new IOException("Unknown status of processs "
+                    + VNC_EXECUTABLE), this);
             return false;
         } else if (myStatus.equals(IoHelper.ProcessStates.RUNNNING)) {
             return true;
@@ -99,7 +104,7 @@ public class VncService extends Y60Service {
             return false;
         }
     }
-    
+
     public void stopServer() throws Exception {
         Logger.v(LOG_TAG, "stopServer");
         if (!isVncServerRunning()) {
@@ -113,7 +118,7 @@ public class VncService extends Y60Service {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, getCurrentNotification());
     }
-    
+
     public void startServer() throws Exception {
         Logger.v(LOG_TAG, "startServer()");
         if (isVncServerRunning()) {
@@ -127,14 +132,16 @@ public class VncService extends Y60Service {
         Process sh = Runtime.getRuntime().exec("su");
         OutputStream os = sh.getOutputStream();
 
-        IoHelper.writeCommand(os, "chmod 777 " + getFilesDir().getAbsolutePath() + "/" + VNC_EXECUTABLE);
+        IoHelper.writeCommand(os, "chmod 777 " + getFilesDir().getAbsolutePath() + "/"
+                + VNC_EXECUTABLE);
         IoHelper.writeCommand(os, myCmdString);
         Logger.v(LOG_TAG, "Starting " + myCmdString);
         Thread.sleep(500);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, getCurrentNotification());
+
     }
-    
+
     static void killServer() throws Exception {
         Process sh;
 
@@ -148,7 +155,7 @@ public class VncService extends Y60Service {
             IoHelper.writeCommand(os, "killall androidvncserver");
             IoHelper.writeCommand(os, "killall -KILL androidvncserver");
             if (IoHelper.findExecutableOnPath("killall") == null) {
-                //showTextOnScreen("I couldn't find the killall executable, please install busybox or i can't stop server");
+                // showTextOnScreen("I couldn't find the killall executable, please install busybox or i can't stop server");
                 Logger.v(LOG_TAG,
                         "I couldn't find the killall executable, please install busybox or i can't stop server");
             }
